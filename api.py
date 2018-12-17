23954182467bc279d98f0bd44c12307133c9645c
Hadrien Hachez <hadrienhachez@hotmail.com>
2018-12-17 11:31:58 +0100

    add procfile

--- api.py
+++ api.py
@@ -72,4 +72,7 @@ def update_comment(_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True,
+            host='0.0.0.0',
+port=port)
